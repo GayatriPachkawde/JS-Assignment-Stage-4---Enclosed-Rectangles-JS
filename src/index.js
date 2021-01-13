d01bd9541f548c9f74bb8983d9680890169148ca
GayatriPachkawde <gayatri5kawde@gmail.com>
2021-01-13 16:59:39 +0530

    fixed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 // function generateCodeFromObject(obj){
 //     //return a code generated string
+
 //    }
 
 module.exports = generateCodeFromObject;
